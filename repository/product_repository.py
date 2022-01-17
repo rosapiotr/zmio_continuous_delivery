3b032c6db0885859b9fd41f5ff9f52871fc1e85e
Piotr Rosa <37775646+rosapiotr@users.noreply.github.com>
2022-01-17 22:49:15 +0100

    Update product_repository.py

--- repository/product_repository.py
+++ repository/product_repository.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 class ProductRepository:
 
     def __init__(self):
-        product_1 = Product("0", "Audi A6", "60000", "BMW")
+        product_1 = Product("0", "Audi A6", "60000", "Audi")
         product_2 = Product("1", "Volkswagen Polo", "25000", "Volkswagen")
         product_3 = Product("2", "Alfa Romeo Giulia", "70000", "Alfa Romeo")
         product_4 = Product("3", "Peugeot 306", "20000", "Peugeot")
