d4f8067f1c482a576592dc7071d4412af90784f5
Krzysztof Mozdzynski <mozdzynk@gmail.com>
2022-01-25 22:43:59 +0100

    Rollback last commit

--- repository/product_repository.py
+++ repository/product_repository.py
@@ -8,7 +8,7 @@ class ProductRepository:
 
     def __init__(self):
         product_1 = Product("0", "Audi A6", "60000", "Audi")
-        product_2 = Product("1", "Fiat Panda", "25000", "Volkswagen")
+        product_2 = Product("1", "Volkswagen Polo", "25000", "Volkswagen")
         product_3 = Product("2", "Alfa Romeo Giulia", "70000", "Alfa Romeo")
         product_4 = Product("3", "Peugeot 306", "20000", "Peugeot")
 
